5841917d7cdcaaf4c5f0c9c72d43d53872a22196
sbpowell19 <sbpowell@umich.edu>
2025-11-18 09:45:04 -0600

    Add files via upload
    Updated code for ma_app.py

--- ma_app.py
+++ ma_app.py
@@ -4,17 +4,15 @@ from create_input_stage1 import stage_one_input
 import numpy as np
 from sklearn.multiclass import OneVsRestClassifier
 from xgboost import XGBClassifier
+import pandas as pd
 
 states = ['AL','AK','AZ','AR','CA','CO','CT','DE', 'FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI',
           'MN','MS','MO','MT','NE','NV','NH','NM','NY','NJ','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT',
           'VT','VA','WA','WV','WI','WY','DC','GU','PR','AS','MP','FM','MH','PW']
 
-incident=['Explosion','Straight-Line Winds','Tidal Wave','Tropical Storm',
-                'Winter Storm','Tsunami','Biological','Coastal Storm','Drought','Earthquake',
-                 'Flood','Freezing','Hurricane','Terrorist','Typhoon','Dam/Levee Break','Chemical',
-                'Mud/Landslide','Nuclear','Severe Ice Storm','Fishing Losses','Crop Losses','Fire',
-                'Snowstorm','Tornado','Civil Unrest', 'Volcanic Eruption','Severe Storm','Toxic Substances',
-                'Human Cause','Other', 'Tropical Depression']
+incident=['Biological','Chemical','Coastal Storm','Dam/Levee Break','Earthquake','Fire','Flood','Hurricane','Mud/Landslide',
+          'Other','Severe Ice Storm','Severe Storm','Snowstorm','Straight-Line Winds','Terrorist','Tornado','Tropical Depression',
+          'Tropical Storm','Typhoon','Volcanic Eruption','Winter Storm']
 
 dec_type=['EM','DR']
 
@@ -31,13 +29,16 @@ if sys_select=='Recommend mission assignments':
     #The following code allows the user to input context with dropdowns.
     #ESFs are suggested based on past data using a pretrained model.
     inc_type=st.multiselect('Please select incident types(s)',
-                           incident)
+                           incident, default=None)
     dec=st.selectbox('Please select declaration type',dec_type)
 
     state=st.selectbox('Please select a state', states)
 
     reg=st.selectbox('Please select region', list(range(1,11)))
 
+    if (inc_type==None):
+        st.markdown('Please select at least one incident type'
+                    )
     X=stage_one_input(inc_type,state,dec,reg)
 
     st.markdown("Suggesting Emergency Support Functions....")
@@ -50,18 +51,21 @@ if sys_select=='Recommend mission assignments':
     #Create lists of suggested ESFs to return to user
     pos_list=[]
     neg_list=[]
-
+    
     for e in esf:
-        if esf_df.loc[:,e]==1:
+        if esf_df.loc[0,e]==1:
             pos_list.append(e)
         else:
             neg_list.append(e)
 
-    st.markdown('''Recommended ESFs:  
-                :green[" ".join(pos_list)]''')
+    pos_esf=" ".join(pos_list)
+    neg_esf=" ".join(neg_list)
+    #st.markdown(pos_esf)
+    st.markdown("Recommended ESFs:")
+    st.markdown(pos_esf)
     
-    st.markdown('''ESFs not suggested:  
-                :yellow[" ".join(neg_list)]''')
+    st.markdown("ESFs not suggested:")  
+    st.markdown(neg_esf)
     
     esf_user_select=st.multiselect('Please select ESFs:', esf[1:])
 
