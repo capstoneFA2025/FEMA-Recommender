c0ceed095eb11d417d2df892f1d776e14c26a62a
sbpowell19 <sbpowell@umich.edu>
2025-11-19 13:48:28 -0600

    Add files via upload
    Updated to include information retrieval and dynamically altering lists based on region selection.

--- ma_app.py
+++ ma_app.py
@@ -5,10 +5,21 @@ import numpy as np
 from sklearn.multiclass import OneVsRestClassifier
 from xgboost import XGBClassifier
 import pandas as pd
+from cap_search import doc_search, build_index
+import nltk
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+nltk.download('stopwords')
+nltk.download('punkt')
 
 states = ['AL','AK','AZ','AR','CA','CO','CT','DE', 'FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI',
           'MN','MS','MO','MT','NE','NV','NH','NM','NY','NJ','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT',
-          'VT','VA','WA','WV','WI','WY','DC','GU','PR','AS','MP','FM','MH','PW']
+          'VT','VA','WA','WV','WI','WY','DC','GU','PR','AS','MP','FM','MH','PW','VI']
+
+reg_state={1:['CT','MA','ME','NH','RI','VT'],2:['NJ','NY','PR','VI'],3:['DC','DE','MD','PA','VA','WV'],4:['AL','FL','GA','KY','MS','NC','SC','TN'],
+           5:['IL','IN','MI','MN','OH','WI'],6:['AR','LA','NM','OK','TX'],7:['IA','KS','MO','NE'],8:['CO','MT','ND','SD','UT','WY'],
+           9:['AS','AZ','CA','FM','GU','HI','MH','MP','NV','PW'],10:['AK','ID','OR','WA']}
 
 incident=['Biological','Chemical','Coastal Storm','Dam/Levee Break','Earthquake','Fire','Flood','Hurricane','Mud/Landslide',
           'Other','Severe Ice Storm','Severe Storm','Snowstorm','Straight-Line Winds','Terrorist','Tornado','Tropical Depression',
@@ -32,16 +43,16 @@ if sys_select=='Recommend mission assignments':
                            incident, default=None)
     dec=st.selectbox('Please select declaration type',dec_type)
 
-    state=st.selectbox('Please select a state', states)
-
     reg=st.selectbox('Please select region', list(range(1,11)))
 
+    state=st.selectbox('Please select a state', reg_state[reg])
+    st.markdown(len(inc_type))
     if (inc_type==None):
         st.markdown('Please select at least one incident type'
                     )
     X=stage_one_input(inc_type,state,dec,reg)
 
-    st.markdown("Suggesting Emergency Support Functions....")
+    #st.markdown("Suggesting Emergency Support Functions....")
     pred=esf_model.predict(X)
 
     pred=pred.reshape(1,-1)
@@ -64,8 +75,8 @@ if sys_select=='Recommend mission assignments':
     st.markdown("Recommended ESFs:")
     st.markdown(pos_esf)
     
-    st.markdown("ESFs not suggested:")  
-    st.markdown(neg_esf)
+    #st.markdown("ESFs not suggested:")  
+    #st.markdown(neg_esf)
     
     esf_user_select=st.multiselect('Please select ESFs:', esf[1:])
 
@@ -79,3 +90,7 @@ if sys_select=='Recommend mission assignments':
 else:
     cap=st.text_input('Please enter a capability',None)
     st.write('The capability is:', cap)
+
+    index, topics = build_index('Topics.txt')
+
+    doc_search(cap, index, topics)
